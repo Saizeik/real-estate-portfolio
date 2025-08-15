d1abb93a2cc189e209503f4e5801063ab0439636
Scott Walker <nswalker44@gmail.com>
2025-08-14 18:29:40 -0700

    layout update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./app/**/*.{js,ts,jsx,tsx}",       // your app directory
-      "./pages/**/*.{js,ts,jsx,tsx}",     // only if using pages/
-      "./components/**/*.{js,ts,jsx,tsx}",// your components
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     safelist: [
       "md:grid-cols-2",
@@ -12,6 +13,10 @@ module.exports = {
       "overflow-hidden",
       "w-full",
       "h-auto",
+      {
+        pattern: /bg-(red|blue|green)-(100|200|500)/, // Safelist patterns using regex
+        variants: ['hover', 'focus'], // Include variants for the pattern
+      },
     ],
     theme: {
       extend: {
